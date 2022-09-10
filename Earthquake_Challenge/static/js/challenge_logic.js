cf2d1119050ccb6abf55fd4040b01b601c13072c
Tammi Gendron <tgendro@gmail.com>
2022-09-10 13:18:52 -0400

    adding deliverable 3

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -23,6 +23,17 @@ let satelliteStreets = L.tileLayer(
   }
 );
 
+// We create the third tile layer that will be the background of our map.
+let lightStreets = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY,
+  }
+);
+
 // Create the map object with center, zoom level and default layer.
 let map = L.map("mapid", {
   center: [40.7, -94.5],
@@ -34,6 +45,7 @@ let map = L.map("mapid", {
 let baseMaps = {
   Streets: streets,
   Satellite: satelliteStreets,
+  Light: lightStreets,
 };
 
 // 1. Add a 2nd layer group for the tectonic plate data.
